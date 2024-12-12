fbe65b20f5521fe8f96593f27836c560d609584b
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-12 11:19:16 +0200

    client components

---
+++ src/app/components/company-form-modal.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react';
+import CompanyForm, { CompanyFormProps } from './company-form';
+import Modal, { ModalProps } from './modal';
+export interface CompanyFormModalProps extends ModalProps {
+  onSubmit: CompanyFormProps['onSubmit'];
+}
+export default function CompanyFormModal({
+  onSubmit,
+  ...rest
+}: CompanyFormModalProps) {
+  return (
+    <Modal {...rest}>
+      <CompanyForm onSubmit={onSubmit} />
+    </Modal>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import StatusLabel, {Status} from "./components/status-label";
+import AddCompanyButton from "./components/add-compnay-button";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+   <h1 className="text-xl">Home page</h1>
+   <AddCompanyButton />
     </main>
   );
 }
