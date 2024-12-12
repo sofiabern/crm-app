85747054e61f08adf8fb15c88798e5e59191fe9b
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-12 23:08:29 +0200

    added sidebar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
-import './globals.css'
+import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import './globals.css';
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import AddCompanyButton from "./components/add-compnay-button";
 export default function Home() {
   return (
     <main>
-   <h1 className="text-xl">Home page</h1>
-   <AddCompanyButton />
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
